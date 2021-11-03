295403a274d5ec75f7e60639305fce655791ef3f
NaumanMunir9 <naumanmunir9@gmail.com>
2021-11-03 14:43:36 +0500

    AppSync API

--- lib/v5-appsync-lambda-dynamodb-stack.ts
+++ lib/v5-appsync-lambda-dynamodb-stack.ts
@@ -1,9 +1,28 @@
 import * as cdk from "@aws-cdk/core";
+import * as appsync from "@aws-cdk/aws-appsync";
+import * as ddb from "@aws-cdk/aws-dynamodb";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class V5AppsyncLambdaDynamodbStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // Creates the AppSync API
+    const api = new appsync.GraphqlApi(this, "MyDynamodbApi", {
+      name: "cdk-appsync-dynamodb-lambda-api",
+      schema: appsync.Schema.fromAsset("graphql/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365)),
+          },
+        },
+      },
+    });
+
+    // lambda function
   }
 }
